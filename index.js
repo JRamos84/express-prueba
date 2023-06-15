dbcec93ee83c9e2d958af1975f8118b95e309110
José <joseph0001@gmail.com>
2023-06-15 16:23:14 -0300

    funcion especial

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express=require('express');
 const routerApi=require('./routes');
+const {logErrors, errorHandler} = require('./middlewares/error.handler')
 const app=express();
-const port=3000;
+const port=3003;
+
 app.use(express.json())
 
 app.get('/',(req,res)=>{res.send('Hola mi server en express');});
 app.get('/nueva-ruta',(req,res)=>{res.send('Hola, soy una nueva ruta');});
 routerApi(app);
+
+app.use(logErrors)
+app.use(errorHandler)
+
 app.listen(port,()=>{console.log('Mi port'+port);})
