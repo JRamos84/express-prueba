1f06d7e6c7b199db46317920f92a763fc6384439
José <joseph0001@gmail.com>
2023-06-22 13:25:11 -0300

    probar vercel

---
+++ api/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const routerApi = require('./routes');
+const cors = require('cors')
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
+const app = express();
+const port = process.env.PORT || 3003;
+
+app.use(express.json())
+
+const whiteList = ['http://localhost:8080']
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('no permitido'))
+    }
+  }
+}
+app.use(cors(options))
+
+app.get('/api', (req, res) => { res.send('Hola mi server en express'); });
+app.get('/api/nueva-ruta', (req, res) => { res.send('Hola, soy una nueva ruta'); });
+routerApi(app);
+
+app.use(logErrors)
+app.use(boomErrorHandler)
+app.use(errorHandler)
+
+app.listen(port, () => { console.log('Mi port' + port); })
